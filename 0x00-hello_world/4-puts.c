a6abdf6a1d9e3aed5220889eb17c5e27fee0c876
selvy <selvyatef2004@gmail.com>
2023-06-15 18:20:43 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- *main Enter point
+ * main - Entry point
  *
- *Description: A C program that print some string with puts function
- *	
- *Return always 0
+ * Description: A C program that prints some string with puts function
+ *
+ * Return: Always 0 (Success)
 */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
