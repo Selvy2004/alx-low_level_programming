f46038ebb45f613fcd163991e3ce28eaea381985
selvy <selvyatef2004@gmail.com>
2023-06-16 17:44:36 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Desctiption: false or true validation
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
