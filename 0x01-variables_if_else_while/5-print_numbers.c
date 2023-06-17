3b96f0daf13ff4115a36d58a412f1d36ad5a5d40
selvy <selvyatef2004@gmail.com>
2023-06-16 18:12:06 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Desctiption: false or true validation
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("n");
+		n++;
+	}
+	printf('\n');
+	return (0);
+}
