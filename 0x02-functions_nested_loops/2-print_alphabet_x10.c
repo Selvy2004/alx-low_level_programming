0922f6690c43bcf3fae84a406a5b2809b5cfb6e9
selvy <selvyatef2004@gmail.com>
2023-06-20 15:35:37 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,9 +10,12 @@
 
 void print_alphabet_x10(void)
 {
-	for (int n = 0 ; n < 10 ; n++)
+	int n;
+	char ch;
+
+	for (n = 0 ; n < 10 ; n++)
 	{
-		for (char ch = 'a' ; ch <= 'z' ; ch++)
+		for (ch = 'a' ; ch <= 'z' ; ch++)
 		{
 			_putchar(ch);
 		}
