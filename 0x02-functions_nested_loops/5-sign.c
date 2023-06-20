e727a80c0bbd4c032fd32c1e4594295d46e03d70
selvy <selvyatef2004@gmail.com>
2023-06-20 16:54:54 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,17 +15,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf("+1\n");
 		return (1);
+		printf("+\n");
 	}
 	else if (n == 0)
 	{
-		printf("0\n");
 		return (0);
+		printf("0\n");
 	}
 	else
 	{
-		printf("-1\n");
 		return (-1);
+		printf("-\n");
 	}
 }
