698f5fce4834ae9d939d4eeb3bdb1b84f878a75a
selvy <selvyatef2004@gmail.com>
2023-06-20 09:34:22 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Desctiption: print alphabet using void print_alphabet
+ * Return: 0 (success)
+*/
+
+void print_alphabet(void);
+
+int main(void)
+{
+	print_alphabet();
+
+	return (0);
+}
+/**
+ * print_alphabet - Prints the lowercase alphabet
+ *
+ * Description: This function prints the lowercase alphabet to the console
+ * using the _putchar function.
+ *
+ * Return: None
+*/
+
+void print_alphabet(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+
+	_putchar('\n');
+}
