ca265547b1f2e4e6cc8fa586dbd7e507d9f1b66a
selvy <selvyatef2004@gmail.com>
2023-06-20 08:46:22 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Desctiption: false or true validation
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	printf("0-putchar.c\n");
+	return (0);
+}
