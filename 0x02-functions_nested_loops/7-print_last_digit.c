5d64b1f58e12a9b5f9766ad0c8ddf858cad12e83
selvy <selvyatef2004@gmail.com>
2023-06-20 17:36:51 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - checks for alphabetic character
+ *
+ * @n: take the input from another
+ *
+ * Return: the value of the last digital
+ *
+*/
+
+int print_last_digit(int n)
+{
+	printf("%i", n % 10);
+}
