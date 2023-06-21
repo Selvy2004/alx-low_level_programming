cd24f9f1d026e334b185ec95846b95c57c334ca6
selvy <selvyatef2004@gmail.com>
2023-06-21 08:13:10 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,33 +8,36 @@
 
 void print_times_table(int n)
 {
-	int i, result;
+	int i, j, result;
 
 	if (n <= 15 && n >= 0)
 	{
 		for (i = 0; i <= n; i++)
 		{
-			result = i * n;
-			if (result < 10)
+			for (j = 0; j <= i; j++)
 			{
-				_putchar(result + '0');
+				result = i * j;
+				if (result < 10)
+				{
+					_putchar(result + '0');
+				}
+				else
+				{
+					_putchar(result / 10 + '0');
+					_putchar(result % 10 + '0');
+				}
+				if (i < n && result < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				if (i < n && result >= 10)
+				{
+					_putchar(' ');
+				}
 			}
-			else
-			{
-				_putchar(result / 10 + '0');
-				_putchar(result % 10 + '0');
-			}
-			if (i < n && result < 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-			}
-			if (i < n && result >= 10)
-			{
-				_putchar(' ');
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
