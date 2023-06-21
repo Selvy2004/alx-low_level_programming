f5ead2af78e3cc7f857b9c2e8c308a19a29ee7a5
selvy <selvyatef2004@gmail.com>
2023-06-20 19:28:32 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - checks for alphabetic character
+ *
+ * @n1: take the input from another
+ * @n2: take the input from another
+ *
+ * Return: the sum of the two inputs
+*/
+
+int add(int n1, int n2)
+{
+	int sum = n1 + n2;
+
+	return (sum);
+}
