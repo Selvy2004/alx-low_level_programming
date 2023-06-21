e1bb946cd1693dc8912d032cbea07f838741c5c7
selvy <selvyatef2004@gmail.com>
2023-06-20 17:30:04 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - checks for alphabetic character
+ *
+ * @n: take the input from another
+ *
+ * Return: 0 (Success)
+ *
+*/
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
