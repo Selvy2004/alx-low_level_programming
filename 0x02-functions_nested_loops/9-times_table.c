7065e860130938ef7437444cc6913b9f6969132f
selvy <selvyatef2004@gmail.com>
2023-06-20 18:49:52 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,14 +7,23 @@
 
 void times_table(void)
 {
-	int n, i;
+	int n, i, result;
 
 	for (n = 0; n < 9; n++)
 	{
 		for (i = 0; i < 9; i++)
 		{
-			_putchar(n * i + '0');
-			if (i > 9)
+			result = n * i;
+			if (result < 10)
+			{
+				_putchar(result + '0');
+			}
+			else
+			{
+				_putchar(result / 10 + '0');
+				_putchar(result % 10 + '0');
+			}
+			if (i < 9)
 			{
 				_putchar(',');
 				_putchar(' ');
