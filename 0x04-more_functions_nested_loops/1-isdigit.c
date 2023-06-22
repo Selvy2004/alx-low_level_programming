7ad3d776bcdbb34c89af97da8def066c00a86528
selvy <selvyatef2004@gmail.com>
2023-06-22 09:08:25 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#inclide "main.h"
+
+/**
+ * _isdigit - checks for alphabetic character
+ *
+ * @c: take the input from another
+ *
+ * Return: 1 if digital bettwen 1 : 9
+ *      or 0 for  any thigh another
+*/
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
