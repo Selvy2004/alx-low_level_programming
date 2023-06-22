c93888937cd02880b50ba2a090289e19d7aa0f91
selvy <selvyatef2004@gmail.com>
2023-06-22 13:34:55 -0700

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: finds and prints the largest prime factor of 612852475143
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	long num = 612852475143;
+	long f = 2;
+	long large_f = 0;
+
+	while (num > 1)
+	{
+		if (num % f == 0)
+		{
+			num /= f;
+			large_f = f;
+
+			while (num / large_f == 0)
+			{
+				num /= large_f;
+			}
+		}
+		f++;
+	}
+	printf("%ld\n", large_f);
+	return (0);
+}
