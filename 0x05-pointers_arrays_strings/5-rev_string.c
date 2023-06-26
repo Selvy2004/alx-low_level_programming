763c6572d8ba5e5eb896acb380491f00e2d4c839
selvy <selvyatef2004@gmail.com>
2023-06-26 16:19:52 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rev_string - reverses a string.
+ *
+ *@s: take the input from another
+*/
+
+void rev_string(char *s)
+{
+	int len = strlen(s);
+	char ch;
+	int i;
+
+	len--;
+
+	for (i = 0; i <= len / 2; i++)
+	{
+		ch = s[i];
+		s[i] = s[len];
+		s[len] = ch;
+		len--;
+	}
+	_putchar('\n');
+}
