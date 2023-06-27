050b02a29cdd1eedafdd4b7c6fbb3625bb37496c
selvy <selvyatef2004@gmail.com>
2023-06-26 17:53:40 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,12 +9,16 @@
 
 void rev_string(char *s)
 {
-	int len = strlen(s);
-	int i;
 
-	for (i = len - 1; i >= 0; i--)
+	int length, i, j;
+
+	length = strlen(s);
+
+	for (i = 0, j = length - 1; i < j; i++, j--)
 	{
-		printf("%c", s[i]);
+		char temp = s[i];
+
+		s[i] = s[j];
+		s[j] = temp;
 	}
-	_putchar('\n');
 }
