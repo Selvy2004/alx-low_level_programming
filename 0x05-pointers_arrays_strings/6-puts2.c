1deae5d9b8906da027808c16154bd248579a9db3
selvy <selvyatef2004@gmail.com>
2023-06-26 18:04:49 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 -  prints every other character of a string
+ *
+ * @str: take the input from another
+ *
+*/
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (*str != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			printf("%c", str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
