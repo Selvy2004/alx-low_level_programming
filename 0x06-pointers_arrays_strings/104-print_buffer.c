d199e2d4cb8b9f6279529b235331cd7851f952e4
selvy <selvyatef2004@gmail.com>
2023-06-29 18:27:46 -0700

    104-print_buffer.c

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * print_buffer - prints a buffer.
+ *
+ * @b: take the input from another
+ * @size: take the input from another
+ *
+*/
+
+void print_buffer(char *b, int size)
+{
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+
+	int i;
+
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%08x ", i);
+		int j;
+
+		for (j = i; j < i + 10; j += 2)
+		{
+			if (j < size)
+			{
+				printf("%02x", (unsigned char)b[j]);
+				if (j + 1 < size)
+					printf("%02x", (unsigned char)b[j + 1]);
+				else
+				{
+					printf("  ");
+				}
+			}
+			else
+				printf("    ");
+			printf(" ");
+		}
+		printf(" ");
+		for (j = i; j < i + 10 && j < size; j++)
+		{
+			if (isprint(b[j]))
+				printf("%c", b[j]);
+			else
+				printf(".");
+		}
+		printf("\n");
+	}
+}
