54d15656869224a6739977722bcfdfdd30ad6fa8
selvy <selvyatef2004@gmail.com>
2023-06-29 18:31:03 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -12,19 +12,16 @@
 
 void print_buffer(char *b, int size)
 {
+	int i, j;
+
 	if (size <= 0)
 	{
 		printf("\n");
 		return;
 	}
-
-	int i;
-
 	for (i = 0; i < size; i += 10)
 	{
 		printf("%08x ", i);
-		int j;
-
 		for (j = i; j < i + 10; j += 2)
 		{
 			if (j < size)
