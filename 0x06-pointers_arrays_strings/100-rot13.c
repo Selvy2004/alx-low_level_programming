2433cbdd675b3e47558d9fc1d7ddface59a9f188
selvy <selvyatef2004@gmail.com>
2023-06-29 17:24:43 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * *rot13 - encodes a string using rot13.
+ *
+ * @str: take the input from another
+ *
+ * Return: str
+*/
+
+char *rot13(char *str)
+{
+	int i, j;
+
+	char rot13_char[53] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+		'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+		'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+		'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	char rot13_shift[53] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+		'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+		'M', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 53; j++)
+		{
+			if (str[i] == rot13_char[j])
+			{
+				str[i] = rot13_shift[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
