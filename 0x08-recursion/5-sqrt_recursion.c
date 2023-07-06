d03911ddcfbf2d5a2a0eeb3ed5aa04a94d20feed
selvy <selvyatef2004@gmail.com>
2023-07-05 17:24:56 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,16 +3,38 @@
 /**
  * _sqrt_recursion - returns the natural square root of a number.
  *
- *@n: takes input from another.
- *
+ * @n: takes input from another.
  * Return: the natural square root of a number.
 */
 
 int _sqrt_recursion(int n)
 {
+	int low = 0, high = n, mid = 0, res = 0;
+
 	if (n < 0)
 	{
 		return (-1);
 	}
-	return (_sqrt_helper(n, 0));
+	if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+	while (low <= high)
+	{
+		mid = (low + high) / 2;
+		if (mid * mid == n)
+		{
+			return (mid);
+		}
+		if (mid * mid < n)
+		{
+			low = mid + 1;
+			res = mid;
+		}
+		else
+		{
+			high = mid - 1;
+		}
+	}
+	return (res);
 }
