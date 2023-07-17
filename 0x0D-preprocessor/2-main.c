dbecc00274c4b84a7a45dc8178fcce9a4686af6e
selvy <selvyatef2004@gmail.com>
2023-07-17 16:22:10 -0700

    1 : 4

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Desctiption: print alphabet using void print_alphabet
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	printf("%s\n", FILE);
+	return (0);
+}
