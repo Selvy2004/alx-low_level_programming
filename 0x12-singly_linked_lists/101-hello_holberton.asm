c2254170af0e15cae69541bcb130a368e89c563b
selvy <selvyatef2004@gmail.com>
2023-07-27 18:21:54 -0700

    101-hello_holberton.

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,31 +1,18 @@
-section .data
-	hello db 'Hello, Holberton', 0
+section	.data
+	hello	db	'Hello, Holberton', 0
+	len	equ	$ - hello
 
-section .text
-	global _start
+section	.text
+	global	asm_start
 
-_start:
-	; Call printf with the hello string as argument
-	; mov rdi, hello ; For x86-64 System V ABI, the first argument goes to rdi
-	; mov eax, 0     ; For x86-64 System V ABI, the syscall number for printf is 0
-	; call printf
-
-	; If you want to use the C library function, you can call main from assembly
-	; with argc and argv as arguments (even though you are not using them).
-	; mov edi, 1     ; argc = 1
-	; lea rsi, [rdi] ; argv = &argc
-	; call main
-
-	; If you want to use the C library function, you can also use the syscall
-	; to invoke printf. Uncomment the following lines to use the syscall.
-	mov rdi, 1     ; File descriptor 1 = standard output
-	lea rsi, [hello]
-	mov rdx, 17    ; Length of the string (excluding null terminator)
-	mov rax, 1     ; Syscall number for sys_write
+asm_start:
+	mov	rax, 1		; syscall number for sys_write
+	mov	rdi, 1		; file descriptor 1 (stdout)
+	mov	rsi, hello	; pointer to the string
+	mov	rdx, len	; length of the string
 	syscall
 
-	; Exit the program
-	mov rax, 60    ; Syscall number for sys_exit
-	xor rdi, rdi   ; Exit status 0
+	mov	rax, 60		; syscall number for sys_exit
+	xor	rdi, rdi	; exit status 0
 	syscall
 
---
+++ 0x12-singly_linked_lists/101-main.c
@@ -0,0 +1,9 @@
+/* 101-main.c */
+
+#include <stdio.h>
+
+int main(void) 
+{
+	printf("This is the main function.\n");
+	return (0);
+}
