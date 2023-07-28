a80c383a581858efeb43ee4b695e53ba2f5895e3
selvy <selvyatef2004@gmail.com>
2023-07-27 18:07:09 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,31 @@
 section .data
-	hello db "Hello, Holberton", 10 ; 10 is the ASCII code for newline
+	hello db 'Hello, Holberton', 0
 
 section .text
 	global _start
 
 _start:
-	; Call printf to print the string
-	mov rdi, hello   ; Load the address of the string into rdi (1st argument for printf)
-	call printf      ; Call the printf function
+	; Call printf with the hello string as argument
+	; mov rdi, hello ; For x86-64 System V ABI, the first argument goes to rdi
+	; mov eax, 0     ; For x86-64 System V ABI, the syscall number for printf is 0
+	; call printf
+
+	; If you want to use the C library function, you can call main from assembly
+	; with argc and argv as arguments (even though you are not using them).
+	; mov edi, 1     ; argc = 1
+	; lea rsi, [rdi] ; argv = &argc
+	; call main
+
+	; If you want to use the C library function, you can also use the syscall
+	; to invoke printf. Uncomment the following lines to use the syscall.
+	mov rdi, 1     ; File descriptor 1 = standard output
+	lea rsi, [hello]
+	mov rdx, 17    ; Length of the string (excluding null terminator)
+	mov rax, 1     ; Syscall number for sys_write
+	syscall
 
 	; Exit the program
-	mov rax, 60      ; syscall number for exit
-	xor rdi, rdi     ; status 0
-	syscall          ; invoke the syscall
+	mov rax, 60    ; Syscall number for sys_exit
+	xor rdi, rdi   ; Exit status 0
+	syscall
+
