469f8972e35d068c544a18fd2ab7750f76213734
selvy <selvyatef2004@gmail.com>
2023-08-07 16:11:12 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,69 +6,120 @@
 #define BUF_SIZE 1024
 
 /**
- * main - Entry point of the program
- * @argc: The number of command-line arguments
- * @argv: An array containing the command-line arguments
- * Return: 0 on success, or an appropriate exit code on failure
+ * display_error - Displays the appropriate error message
+ * @code: The error code
+ * @file: The name of the file causing the error
  */
-int main(int argc, char *argv[])
+void display_error(int code, char *file)
 {
-	int fd_from, fd_to;
+	switch (code)
+	{
+	case 97:
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		break;
+	case 98:
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
+		break;
+	case 99:
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+		break;
+	case 100:
+		dprintf(STDERR_FILENO, "Error: Can't close fd\n");
+		break;
+	}
+}
+
+/**
+ * close_file - Closes the file descriptor
+ * @fd: The file descriptor
+ * @file: The name of the file being processed
+ * Return: 0 on success, 100 on failure
+ */
+int close_file(int fd, char *file)
+{
+	if (close(fd) == -1)
+	{
+		display_error(100, file);
+		return (100);
+	}
+	return (0);
+}
+
+/**
+ * copy_file - Copies the content from one file to another
+ * @fd_from: The file descriptor of the source file
+ * @fd_to: The file descriptor of the destination file
+ * @file_from: The name of the source file
+ * @file_to: The name of the destination file
+ * Return: 0 on success, the appropriate error code on failure
+ */
+int copy_file(int fd_from, int fd_to, char *file_from, char *file_to)
+{
 	ssize_t ret;
 	char buffer[BUF_SIZE];
-	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
-
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
-		return (97);
-	}
-
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		return (98);
-	}
-
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
-	if (fd_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		close(fd_from);
-		return (99);
-	}
 
 	while ((ret = read(fd_from, buffer, BUF_SIZE)) > 0)
 	{
 		if (write(fd_to, buffer, ret) != ret)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(fd_from);
-			close(fd_to);
+			display_error(99, file_to);
+			close_file(fd_from, file_from);
+			close_file(fd_to, file_to);
 			return (99);
 		}
 	}
 
 	if (ret == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		close(fd_from);
-		close(fd_to);
+		display_error(98, file_from);
+		close_file(fd_from, file_from);
+		close_file(fd_to, file_to);
 		return (98);
 	}
 
-	if (close(fd_from) == -1)
+	return (0);
+}
+
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: An array containing the command-line arguments
+ * Return: 0 on success, the appropriate error code on failure
+ */
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to;
+	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+
+	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		return (100);
+		display_error(97, NULL);
+		return (97);
 	}
 
-	if (close(fd_to) == -1)
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		return (100);
+		display_error(98, argv[1]);
+		return (98);
 	}
+
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
+	if (fd_to == -1)
+	{
+		display_error(99, argv[2]);
+		close_file(fd_from, argv[1]);
+		return (99);
+	}
+
+	if (copy_file(fd_from, fd_to, argv[1], argv[2]) != 0)
+		return (99);
+
+	if (close_file(fd_from, argv[1]) != 0)
+		return (100);
+
+	if (close_file(fd_to, argv[2]) != 0)
+		return (100);
 
 	return (0);
 }
