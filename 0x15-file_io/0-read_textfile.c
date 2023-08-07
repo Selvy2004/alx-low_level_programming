1630068429f0db24c6ae5ea5de911a459d3a0e3d
selvy <selvyatef2004@gmail.com>
2023-08-07 16:19:44 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,33 +5,32 @@
 #include "main.h"
 
 /**
- * read_textfile - Reads a text file
- * and prints it to the POSIX standard output.
+ * read_textfile - Reads a text file and prints
+ * it to the POSIX standard output.
  * @filename: The name of the file to read
  * @letters: The number of letters to read and print
  * Return: The actual number of letters read and printed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t n_read, n_written;
-	char *buffer;
-
 	if (filename == NULL)
 		return (0);
 
-	buffer = malloc(sizeof(char) * (letters + 1));
-	if (buffer == NULL)
+	int fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	char *buffer = malloc(sizeof(char) * letters);
+
+	if (buffer == NULL)
 	{
-		free(buffer);
+		close(fd);
 		return (0);
 	}
 
-	n_read = read(fd, buffer, letters);
+	ssize_t n_read = read(fd, buffer, letters);
+
 	if (n_read == -1)
 	{
 		free(buffer);
@@ -39,9 +38,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
-	buffer[n_read] = '\0';
+	ssize_t n_written = write(STDOUT_FILENO, buffer, n_read);
 
-	n_written = write(STDOUT_FILENO, buffer, n_read);
 	if (n_written != n_read)
 	{
 		free(buffer);
